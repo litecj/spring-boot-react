d39e4a502f6c42e6588d74288e74bf2072335269
litecj <litecj702@gmail.com>
2021-10-23 16:21:35 +0900

    d6_2

--- frontend/src/features/common/components/Home.js
+++ frontend/src/features/common/components/Home.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { useHistory } from 'react-router-dom';
 
 export default function Home() {
-return (<>
-    <h1>Home</h1>
-</>)
+    const sessionUser = localStorage.getItem("sessionUser")
+    const history = useHistory()
+    const logout = e => {
+        e.preventDefault()
+        localStorage.setItem('sessionUser','')
+        history.push('/')
+    }
+
+    return (<>
+        {sessionUser !== '' && <input type="button" value="로그아웃" onClick={logout}/> }
+        {sessionUser === '' && <input type="button" value="로그인" onClick={e => history.push('/users/login')}/> }
+        <h1>Home</h1>
+        {sessionUser !== '' ? <h1>{sessionUser.username} 안녕 :) 로그인 중이네? ...</h1>:<><button onClick = {e => history.push('/users/add')}>회원가입</button><button onClick = {e => history.push('/users/login')}>로그인</button></>}
+    </>)
 }
