f0b6ed65f795284eaeedfceb804790e98f220347
litecj <litecj702@gmail.com>
2021-10-25 20:05:51 +0900

    h2

--- frontend/src/features/user/index.js
+++ frontend/src/features/user/index.js
@@ -1,3 +1,4 @@
+// user components
 export { default as UserAdd } from 'features/user/components/UserAdd'
 export { default as UserDetail } from 'features/user/components/UserDetail'
 export { default as UserList } from 'features/user/components/UserList'
@@ -6,3 +7,10 @@ export { default as UserModify } from 'features/user/components/UserModify'
 export { default as UserRemove } from 'features/user/components/UserRemove'
 export { default as UserListForm} from 'features/user/components/UserListForm'
 // export { User } from 'features/user/User'
+
+
+// user reducer
+export {default as userReucer} from 'features/user/userSlice'
+
+// user api
+export {default as userAPI} from 'features/user/userAPI'
